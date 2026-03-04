799ce45ad316fd9edda80bf96105e43e4cbb7054
Débora Serradilha <deboraserradilhadefreitas@gmail.com>
2026-03-03 21:37:14 -0300

    Alteração de cadastro filhotes

--- src/app/pages/filhotes/filhotes.module.ts
+++ src/app/pages/filhotes/filhotes.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { FilhotesComponent } from './filhotes.component';
 import { FilhotesRoutingModule } from './filhotes-routing.module';
@@ -11,8 +10,7 @@ import { FilhotesRoutingModule } from './filhotes-routing.module';
   ],
   imports: [
     CommonModule,
-    FilhotesRoutingModule,
-    ReactiveFormsModule
+    FilhotesRoutingModule
   ]
 })
 export class FilhotesModule { }
